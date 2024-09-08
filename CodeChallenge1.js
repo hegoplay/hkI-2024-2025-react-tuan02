a83e4d9d9580dc9d332ca8db96f56a6eb992bde1
Pham The Manh <thmnh113@gmail.com>
2024-09-08 13:05:18 +0700

    init

---
+++ CodeChallenge1.js
@@ -0,0 +1,8 @@
+const calcBMI = (mass, height) => mass/(height*height);
+
+let markHigherBMI =  calcBMI(78, 1.69) > calcBMI(92, 1.95);
+// let markHigherBMI =  calcBMI(95, 1.88) > calcBMI(85, 1.76);
+
+console.log(markHigherBMI );
+
+
